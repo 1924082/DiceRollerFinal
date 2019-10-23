8c8aa7c1ffa259a35a4a56455eeec7b6549d1e27
Julia <1924082@brunel.ac.uk>
2019-10-23 16:01:43 +0100

    Final version of Dice Roller.

--- app/src/main/java/com/example/a1924082/diceroller/DiceRoller.java
+++ app/src/main/java/com/example/a1924082/diceroller/DiceRoller.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -60,20 +62,48 @@ import java.util.Random;
 
         tv.setText(Integer.toString(txtNoNumberYet));
 
+        TextView tv2 = (TextView) this.findViewById(R.id.ResultMessage);
 
+        //method1
+        EditText mEdit;
+        mEdit = (EditText) this.findViewById(R.id.EnterNumber);
+
+        Editable guessedText;
+
+
+
+        if (mEdit.getText().toString() !=""){
+            try {
+                guessedText = mEdit.getText();
+
+                //method2
+
+                //parsing to int
+                int guessme = Integer.parseInt(guessedText.toString());
+
+                TextView tv3 = (TextView) this.findViewById(R.id.Points);
+
+                if (guessme == txtNoNumberYet) {
+                    tv2.setText("Congratulation you guessed the right number!");
+                    score += 1;
+                    tv3.setText("Your score is: " + score);
+                } else {
+                    tv2.setText("You did not guessed the right number!");
+                }
+            }
+            catch(Exception ex){
+
+                    Toast.makeText(DiceRoller.this,
+                            "Error" + ex.toString(),
+                            Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
 
     }
 
-
-
-
-
-
-     }
-
-
-
-
+     private int score;
 
 
 }
